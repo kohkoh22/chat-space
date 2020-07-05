9dbc853b803c33a0095722b6bd6b9c24f8c4270b
kohkoh22 <kohnosuke@gmail.com>
2020-07-05 16:04:34 +0900

    Revert "Merge pull request #11 from kohkoh22/ajax-auto-function"
    This reverts commit 45c48575946c8691301bebfed2ac4ef42182ecbf, reversing
    changes made to ecfdf40476de16b2d5f9c04608a7e3d7e58b92a3.

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,64 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+      `<div class="mainchat__messagelist--box">
+        <div class="mainchat__messagelist--box--name">
+          ${message.user_name}
+        </div>
+        <div class="mainchat__messagelist--box--date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="mainchat__messagelist--message">
+        <p class="mainchat__messagelist--message--content">
+         ${message.content}
+        </p>
+        <img class="Message__image" src="${message.image}">
+      </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="mainchat__messagelist--box">
+        <div class="mainchat__messagelist--box--name">
+          ${message.user_name}
+        </div>
+        <div class="mainchat__messagelist--box--date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="mainchat__messagelist--message">
+        <p class="mainchat__messagelist--message--content">
+         ${message.content}
+        </p>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.form_all').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.mainchat__messagelist').append(html);      
+      $('form')[0].reset();
+      $('.mainchat__messagelist').animate({ scrollTop: $('.mainchat__messagelist')[0].scrollHeight});
+      $('.formbtn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+      $('.formbtn').prop('disabled', false);
+    });
+
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:update, :edit, :index]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-#追加
-    namespace :api do
-      resources :messages, only: :index, defaults: { format: 'json' }
-    end
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
